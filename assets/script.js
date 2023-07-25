3b948812243815066a98f78f2f7a721759482cd0
Joan <joan.vigne@hotmail.fr>
2023-07-25 13:10:42 +0100

    initial commit, console.log de arrow left and right

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+const arrow = document.getElementsByClassName("arrow");
+const arrow_left = arrow[0];
+const arrow_right = arrow[1];
+
+arrow_left.addEventListener("click", () => console.log("ok left"));
+arrow_right.addEventListener("click", () => console.log("ok right"));
