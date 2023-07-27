db64047699b4cf0b86586269fc781d633daaf012
Joan <joan.vigne@hotmail.fr>
2023-07-27 10:04:18 +0100

    premiere version de function droite gauche

--- assets/script.js
+++ assets/script.js
@@ -21,8 +21,8 @@ const slides = [
 const arrow = document.getElementsByClassName("arrow");
 const arrow_left = arrow[0];
 const arrow_right = arrow[1];
-arrow_left.addEventListener("click", () => console.log("ok left"));
-arrow_right.addEventListener("click", () => console.log("ok right"));
+arrow_left.addEventListener("click", () => changeImageGauche());
+arrow_right.addEventListener("click", () => changeImageDroite());
 // fin EVENT SUR LES FLECHES
 
 //* creation des bullet points
@@ -34,9 +34,79 @@ for (let i = 0; i < slides.length; i++) {
 }
 //* .dot_selected
 const lesDots = document.getElementsByClassName("dot");
-console.log("les dots: ", lesDots);
+
 lesDots[0].classList.add("dot_selected");
 /* lesDots[0].classList.remove("dot_selected"); */
 
 const imgEnCours = document.getElementsByClassName("banner-img")[0];
-console.log(imgEnCours.src);
+console.log(imgEnCours.nextElementSibling);
+
+//* changer d'image
+
+function changeImageDroite() {
+  switch (imgEnCours.src) {
+    case `http://localhost:8080/assets/images/slideshow/slide1.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide2.jpg`;
+      lesDots[0].classList.remove("dot_selected");
+      lesDots[1].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[1].tagLine;
+      break;
+
+    case `http://localhost:8080/assets/images/slideshow/slide2.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide3.jpg`;
+      lesDots[1].classList.remove("dot_selected");
+      lesDots[2].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[2].tagLine;
+      break;
+    case `http://localhost:8080/assets/images/slideshow/slide3.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide4.png`;
+      lesDots[2].classList.remove("dot_selected");
+      lesDots[3].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[3].tagLine;
+      break;
+
+    case `http://localhost:8080/assets/images/slideshow/slide4.png`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide1.jpg`;
+      lesDots[3].classList.remove("dot_selected");
+      lesDots[0].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[0].tagLine;
+      break;
+    default:
+      console.log("error");
+      break;
+  }
+}
+
+function changeImageGauche() {
+  switch (imgEnCours.src) {
+    case `http://localhost:8080/assets/images/slideshow/slide1.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide4.png`;
+      lesDots[0].classList.remove("dot_selected");
+      lesDots[3].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[3].tagLine;
+      break;
+
+    case `http://localhost:8080/assets/images/slideshow/slide2.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide1.jpg`;
+      lesDots[1].classList.remove("dot_selected");
+      lesDots[0].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[0].tagLine;
+      break;
+    case `http://localhost:8080/assets/images/slideshow/slide3.jpg`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide2.jpg`;
+      lesDots[2].classList.remove("dot_selected");
+      lesDots[1].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[1].tagLine;
+      break;
+
+    case `http://localhost:8080/assets/images/slideshow/slide4.png`:
+      imgEnCours.src = `http://localhost:8080/assets/images/slideshow/slide3.jpg`;
+      lesDots[3].classList.remove("dot_selected");
+      lesDots[2].classList.add("dot_selected");
+      imgEnCours.nextElementSibling.innerHTML = slides[2].tagLine;
+      break;
+    default:
+      console.log("error");
+      break;
+  }
+}
